5fc3ac565aa845b6dfc3a70932ed8cce30fa8f59
Shah-Jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-03-08 13:03:07 -0800

    TODO

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Task from './compenents/Task';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+import Task from "./compenents/Task";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Task />
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ marginTop: 25 }}>
+      <View style={styles.container}>
+        <Task name="Task" />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
-   
+    backgroundColor: "#DCDCDC",
+    width: "100%",
+    height: "100%",
   },
 });
--- compenents/Task.js
+++ compenents/Task.js
@@ -1,11 +1,47 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 
-const Task = () => {
+const Task = (props) => {
   return (
-    <View>
-      <View style={{ width: 10, height: 90 }}> </View>
-    </View>
+    <SafeAreaView>
+      <View>
+        <View style={{ padding: 50 }}>
+          <Text>TODO list</Text>
+        </View>
+        <View
+          style={{
+            width: "90%",
+            height: 50,
+            backgroundColor: "white",
+            justifyContent: "center",
+            alignItems: "center",
+            marginLeft: "5%",
+            borderRadius: 10,
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              marginRight: "75%",
+              marginLeft: 10,
+              width: 20,
+              height: 20,
+              borderWidth: 5,
+              borderRadius: 5,
+              borderColor: "#0000A5",
+            }}
+          ></View>
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text style={{ fontWeight: "bold" }}>{props.name}</Text>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
