3e1039d22a67e8b441895961cd47048f276b3931
Shah-Jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-03-08 12:34:17 -0800

    TODO

--- compenents/Task.js
+++ compenents/Task.js
@@ -1,13 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const Task = () => {
   return (
     <View>
-      <View style={{width:10,height:90}} > </View>
-     
+      <View style={{ width: 10, height: 90 }}> </View>
     </View>
-  )
-}
+  );
+};
 
 export default Task;
